64c9f4cc0be25feeda05b8462dd790a7989e604f
falamarcao <falamarcao@gmail.com>
2024-10-24 01:49:41 -0300

    Rendering Content Conditionall

--- src/components/Examples/index.tsx
+++ src/components/Examples/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 
 import TabButton from '../TabButton';
 
@@ -9,11 +9,24 @@ import { EXAMPLES } from '../../data/Examples';
 import './Examples.css';
 
 const Examples = () => {
-  const [selectedTab, setSelectedTab] = useState<string>(TabName.Components);
+  const [selectedTab, setSelectedTab] = useState<TabName>();
 
   function handleSelect(tabName: TabName) {
     setSelectedTab(tabName);
   }
+
+  let content: ReactNode = <p>Please select a topic</p>;
+
+  if (selectedTab)
+    content = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTab].title}</h3>
+        <p>{EXAMPLES[selectedTab].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTab].code}</code>
+        </pre>
+      </div>
+    );
 
   return (
     <section id="examples">
@@ -31,13 +44,7 @@ const Examples = () => {
           State
         </TabButton>
       </menu>
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTab].title}</h3>
-        <p>{EXAMPLES[selectedTab].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTab].code}</code>
-        </pre>
-      </div>
+      {content}
     </section>
   );
 };
